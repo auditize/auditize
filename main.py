1cef546307ba6b6b038b2fbeca96fa82d78bba12
Nicolas Delon <nicolas.delon@gmail.com>
2024-03-04 13:40:11 +0100

    Uber-basic implementation of POST /logs

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+from datetime import datetime
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import motor.motor_asyncio
+
+
+mongo = motor.motor_asyncio.AsyncIOMotorClient()
+db = mongo.get_database("auditize")
+log_collection = db.get_collection("logs")
+
+
+class Event(BaseModel):
+    name: str = Field(...)
+    category: str
+
+
+class LogBase(BaseModel):
+    event: Event
+    occurred_at: datetime
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World !"}
+@app.post("/logs")
+async def create_log(log: LogBase):
+    result = await log_collection.insert_one(log.model_dump())
+    return {"id": str(result.inserted_id)}
