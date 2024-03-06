681d81373b21c871c96621631c2cf236e1695b78
Nicolas Delon <nicolas.delon@gmail.com>
2024-03-06 11:22:39 +0100

    Implement GET /log/{id} + improvements

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from datetime import datetime
+from bson.objectid import ObjectId
+from typing import Annotated
 
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, BeforeValidator
 import motor.motor_asyncio
 
 
 mongo = motor.motor_asyncio.AsyncIOMotorClient()
 db = mongo.get_database("auditize")
 log_collection = db.get_collection("logs")
+
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class Event(BaseModel):
@@ -20,10 +25,41 @@ class LogBase(BaseModel):
     occurred_at: datetime
 
 
+class Log(LogBase):
+    id: PyObjectId = Field(validation_alias="_id")
+
+
+class LogCreation(LogBase):
+    pass
+
+
+class LogCreated(BaseModel):
+    id: str
+
+
+async def save_log(log: LogCreation) -> str:
+    result = await log_collection.insert_one({
+        **log.model_dump(),
+        "created_at": datetime.utcnow(),
+    })
+    return str(result.inserted_id)
+
+
+async def get_log(log_id: str) -> Log:
+    data = await log_collection.find_one(ObjectId(log_id))
+    return Log(**data)
+
+
 app = FastAPI()
 
 
-@app.post("/logs")
-async def create_log(log: LogBase):
-    result = await log_collection.insert_one(log.model_dump())
-    return {"id": str(result.inserted_id)}
+@app.post("/logs", status_code=201)
+async def create_log(log: LogCreation) -> LogCreated:
+    log_id = await save_log(log)
+    return LogCreated(id=log_id)
+
+
+@app.get("/logs/{log_id}")
+async def read_log(log_id: str) -> Log:
+    log = await get_log(log_id)
+    return log
