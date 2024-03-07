136204a00df6ed8b36ef825d587ab7677c7478e5
Nicolas Delon <nicolas.delon@gmail.com>
2024-03-07 20:54:54 +0100

    tests: fix broken event loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,13 @@ from httpx import AsyncClient, ASGITransport
 from main import app
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def anyio_backend():
     # Limit the tests to only run on asyncio:
     return 'asyncio'
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 async def client():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost") as client:
         yield client
