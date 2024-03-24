49e8274a1a786b72d3d479bfae8c7a622ce51527
Nicolas Delon <nicolas.delon@gmail.com>
2024-03-24 22:03:08 +0100

    front: introduce tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { PrimeReactProvider } from 'primereact/api';
+import { twMerge } from 'tailwind-merge';
+import Tailwind from 'primereact/passthrough/tailwind';
 import LogList from './components/LogList';
 
 import {
   QueryClient,
   QueryClientProvider,
-  useQuery,
 } from '@tanstack/react-query'
 
 import './App.css'
-import 'primereact/resources/themes/lara-light-blue/theme.css';
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 function App() {
   const queryClient = new QueryClient()
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <LogList />
-    </QueryClientProvider>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind, ptOptions: { mergeSections: true, mergeProps: true, classNameMergeFunction: twMerge } }}>
+      <QueryClientProvider client={queryClient}>
+        <LogList />
+      </QueryClientProvider>
+    </PrimeReactProvider>
   )
 }
 
