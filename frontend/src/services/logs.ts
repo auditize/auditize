c55ca395d7eef2e67bc0d6dbc0ce4715089fcdd8
Nicolas Delon <nicolas.delon@gmail.com>
2024-03-26 11:44:46 +0100

    front: simulate latency while retrieving logs

--- frontend/src/services/logs.ts
+++ frontend/src/services/logs.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+async function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export async function getLogs(cursor: string | null, limit = 3): Promise<{logs: Log[], nextCursor: string | null}> {
+  await timeout(300);  // FIXME: Simulate network latency
+
   const response = await axios.get('http://localhost:8000/logs', {
     params: {
       limit,
