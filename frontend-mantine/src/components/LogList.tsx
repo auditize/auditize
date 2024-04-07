e8281e3a4b80686f10091446c3abf664bf2bc9f8
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-07 23:01:33 +0200

    frontend-mantine: log details modal improvements

--- frontend-mantine/src/components/LogList.tsx
+++ frontend-mantine/src/components/LogList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState, useReducer } from 'react';
 import { useDisclosure } from '@mantine/hooks';
-import { rem, Table, Button, Center, Container, Select, Group, Stack, TextInput, Popover, Space, Anchor, Modal } from '@mantine/core';
+import { rem, Table, Button, Center, Container, Select, Group, Stack, TextInput, Popover, Space, Anchor, Modal, Title, Text, Divider, Accordion } from '@mantine/core';
 import { DateTimePicker } from '@mantine/dates';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { labelize } from './utils';
@@ -8,7 +8,7 @@ import { getLogs, getAllLogEventNames, getAllLogEventCategories, getAllLogActorT
 import 'rsuite/dist/rsuite-no-reset.min.css';
 import { Tree } from 'rsuite';
 import { ItemDataType } from 'rsuite/esm/@types/common';
-import { IconChevronUp, IconChevronDown } from '@tabler/icons-react';
+import { IconChevronUp, IconChevronDown, IconUser, IconCylinder, IconHierarchy } from '@tabler/icons-react';
 
 
 function iconSize(size: any) {
@@ -32,23 +32,62 @@ const emptyLogFilterParams: LogFilterParams = {
 
 function LogDetails({log, opened, onClose}: {log: Log, opened: boolean, onClose: () => void}) {
   return (
-    <Modal title="Log details" size="md" padding="lg" opened={opened} onClose={onClose}>
+    <Modal
+      title={<Title order={1}>Log details</Title>} size="lg" padding="lg"
+      opened={opened} onClose={onClose}
+      >
       <div>
-        <p>Event: {log.event.name}</p>
-        <p>Category: {log.event.category}</p>
-        {
-          log.actor ?
-            <p>Actor: {log.actor.name} ({log.actor.type})</p> :
-            null
-        }
-        {
-          log.resource ?
-            <p>Resource: {log.resource.name} ({log.resource.type})</p> :
-            null
-        }
-        <p>Node: {
-          log.node_path.map((node) => node.name).join(' > ')
-        }</p>
+        <table>
+          <tbody>
+            <tr>
+              <td><Text fw={700}>Date</Text></td>
+              <td>{log.saved_at}</td>
+            </tr>
+            <tr>
+              <td><Text fw={700}>Event name</Text></td>
+              <td>{labelize(log.event.name)}</td>
+            </tr>
+            <tr>
+              <td><Text fw={700}>Event category</Text></td>
+              <td>{labelize(log.event.category)}</td>
+            </tr>
+          </tbody>
+        </table>
+        <Divider my="md" size="md" color="blue"/>
+        <Accordion multiple defaultValue={["actor", "resource", "node"]} variant='default'>
+          <Accordion.Item key="actor" value="actor">
+            <Accordion.Control icon={<IconUser style={iconSize("1.15rem")}/>}>
+              Actor
+            </Accordion.Control>
+            <Accordion.Panel>
+              {
+                log.actor ?
+                <p>{log.actor.name} ({labelize(log.actor.type)})</p> :
+                null
+              }
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item key="resource" value="resource">
+            <Accordion.Control icon={<IconCylinder style={iconSize("1.15rem")}/>}>
+              Resource
+            </Accordion.Control>
+            <Accordion.Panel>
+              {
+                log.resource ?
+                <p>{log.resource.name} ({labelize(log.resource.type)})</p> :
+                null
+              }
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item key="node" value="node">
+            <Accordion.Control icon={<IconHierarchy style={iconSize("1.15rem")}/>}>
+              Node
+            </Accordion.Control>
+            <Accordion.Panel>
+              {log.node_path.map((node) => node.name).join(' > ')}
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </Modal>
   );
