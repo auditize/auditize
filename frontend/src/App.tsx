63902d240b15a2aefe35ff843f43f33003173547
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-10 17:13:46 +0200

    front: introduce react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,27 +7,53 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+
+function Main() {
+  return (
+    <AppShell header={{height: 60}}>
+      <AppShell.Header>
+        <Group h="100%" px="md" justify="right">
+          <Group ml="xl">
+            <UnstyledButton>
+              <Link to="/logs">Logs</Link>
+            </UnstyledButton>
+          </Group>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  );
+}
 
 export default function App() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+      children: [
+        {
+          path: "/logs",
+          element: <Logs/>
+        }
+      ]
+    }
+  ]);
 
   return (
     <MantineProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <AppShell
-          header={{height: 60}}
-        >
-          <AppShell.Header>
-            <Group h="100%" px="md" justify="right">
-              <Group ml="xl">
-                <UnstyledButton>Logs</UnstyledButton>
-              </Group>
-            </Group>
-          </AppShell.Header>
-          <AppShell.Main>
-            <Logs />
-          </AppShell.Main>
-        </AppShell>
+        <RouterProvider router={router}/>
       </QueryClientProvider>
     </MantineProvider>
   );
