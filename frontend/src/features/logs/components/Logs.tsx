14dd2dbefb39c85bd17f7333efb9e9e75efa0cfa
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-17 14:20:17 +0200

    front: implement query string routing for logs UI

--- frontend/src/features/logs/components/Logs.tsx
+++ frontend/src/features/logs/components/Logs.tsx
@@ -1,18 +1,46 @@
-import { useState } from 'react';
 import { Container } from '@mantine/core';
 import { LogsFilterParams } from '../api';
 import { LogsLoader } from './LogsLoader';
 import { LogsFilter } from './LogsFilter';
-import { buildEmptyLogsFilterParams } from '../api';
+import { useSearchParams } from 'react-router-dom';
+import { deserializeDate, serializeDate } from '@/utils/date';
+
+function searchParamsToFilter(params: URLSearchParams): LogsFilterParams {
+  const obj = Object.fromEntries(params.entries());
+  return {
+    ...obj,
+    since: obj.since ? deserializeDate(obj.since) : null,
+    until: obj.until ? deserializeDate(obj.until) : null
+  }
+}
+
+function stripEmptyStringsFromObject(obj: any): any {
+  return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value !== ""));
+}
+
+function filterToSearchParams(filter: LogsFilterParams): URLSearchParams {
+  return new URLSearchParams(stripEmptyStringsFromObject({
+    ...filter,
+    since: filter.since ? serializeDate(filter.since) : "",
+    until: filter.until ? serializeDate(filter.until) : ""
+  }));
+}
 
 export function Logs() {
-  const [filter, setFilter] = useState<LogsFilterParams>(buildEmptyLogsFilterParams());
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filter = searchParamsToFilter(searchParams);
 
   return (
     <Container size="xl" p="20px">
-      <LogsFilter params={filter} onChange={setFilter} />
+      <LogsFilter params={filter} onChange={(newFilter) => setSearchParams(filterToSearchParams(newFilter))} />
       <LogsLoader filter={filter} onTableFilterChange={(name, value) => {
-        setFilter((filter) => ({repoId: filter.repoId, since: filter.since, until: filter.until, [name]: value}));
+        setSearchParams((currentSearchParams) => {
+          const currentFilter = searchParamsToFilter(currentSearchParams);
+          const newFilter = {
+            repoId: currentFilter.repoId, since: currentFilter.since, until: currentFilter.until, [name]: value
+          };
+          return filterToSearchParams(newFilter);
+        })
       }}/>
     </Container>
   );
