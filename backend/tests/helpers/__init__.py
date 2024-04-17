f4b7afda18a0b6c6bcf4f3ff6c4c00f712f538b5
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-17 22:32:38 +0200

    tests: improve usage of HttpTestClient in 'helpers'

--- backend/tests/helpers/__init__.py
+++ backend/tests/helpers/__init__.py
@@ -53,41 +53,48 @@ async def do_test_page_pagination_common_scenarios(client: HttpTestHelper, path:
     This function assumes that for the given path (with possible query string), the total number of items is 5.
     """
     # first test, without pagination parameters
-    resp = await client.assert_get(path)
-    assert resp.json() == {
-        "data": data,
-        "pagination": {
-            "page": 1,
-            "page_size": 10,
-            "total": 5,
-            "total_pages": 1
+    await client.assert_get(
+        path,
+        expected_json={
+            "data": data,
+            "pagination": {
+                "page": 1,
+                "page_size": 10,
+                "total": 5,
+                "total_pages": 1
+            }
         }
-    }
+    )
 
     # second test, with pagination parameters
-    resp = await client.assert_get(path, params={"page": 2, "page_size": 2})
-    assert resp.json() == {
-        "data": data[2:4],
-        "pagination": {
-            "page": 2,
-            "page_size": 2,
-            "total": 5,
-            "total_pages": 3
+    await client.assert_get(
+        path,
+        params={"page": 2, "page_size": 2},
+        expected_json={
+            "data": data[2:4],
+            "pagination": {
+                "page": 2,
+                "page_size": 2,
+                "total": 5,
+                "total_pages": 3
+            }
         }
-    }
+    )
 
 
 async def do_test_page_pagination_empty_data(client: HttpTestHelper, path: str):
-    resp = await client.assert_get(path)
-    assert resp.json() == {
-        "data": [],
-        "pagination": {
-            "page": 1,
-            "page_size": 10,
-            "total": 0,
-            "total_pages": 0
+    await client.assert_get(
+        path,
+        expected_json={
+            "data": [],
+            "pagination": {
+                "page": 1,
+                "page_size": 10,
+                "total": 0,
+                "total_pages": 0
+            }
         }
-    }
+    )
 
 
 def make_log_data(extra=None) -> dict:
