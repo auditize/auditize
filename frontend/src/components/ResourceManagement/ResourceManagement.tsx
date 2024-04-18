ab09264644796ccd29d6c343ff67f99df71fd4e6
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-18 11:32:17 +0200

    front: refactor ReposManagement component to extract a generic ResourceManagement component

---
+++ frontend/src/components/ResourceManagement/ResourceManagement.tsx
@@ -0,0 +1,137 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { addQueryParamToLocation } from "@/utils/router";
+import { Table, Anchor, Divider, Pagination, Button } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useLocation, Link, useSearchParams, useNavigate } from "react-router-dom";
+
+function ResourceTableRow(
+  {
+    resourceName, resource, rowValueBuilders, resourceDeletionComponentBuilder,
+  }: {
+    resourceName: string;
+    resource: any;
+    rowValueBuilders: ((resource: any) => React.ReactNode)[];
+    resourceDeletionComponentBuilder: (
+      resource: any, opened: boolean, onClose: () => void
+    ) => React.ReactNode;
+  }) {
+  const location = useLocation();
+  const resourceLink = addQueryParamToLocation(location, resourceName, resource.id);
+  const [deletionModalOpened, { open: openDeletionModal, close: closeDeletionModal }] = useDisclosure();
+
+  return (
+    <Table.Tr>
+      <Table.Td>
+        <Anchor component={Link} to={resourceLink}>
+          {resource.id}
+        </Anchor>
+      </Table.Td>
+      {rowValueBuilders.map(
+        (builder, i) => (
+          <Table.Td key={i}>{builder(resource)}</Table.Td>
+        )
+      )}
+      <Table.Td>
+        <Anchor component={Link} to={resourceLink}>
+          Edit
+        </Anchor>
+        <Divider size="sm" orientation="vertical" />
+        <Anchor onClick={() => openDeletionModal()}>
+          Delete
+        </Anchor>
+        {resourceDeletionComponentBuilder(resource, deletionModalOpened, closeDeletionModal)}
+      </Table.Td>
+    </Table.Tr>
+  );
+}
+
+export function ResourceManagement(
+  {
+    title,
+    path,
+    resourceName,
+    queryKey,
+    queryFn,
+    columnBuilders,
+    resourceCreationComponentBuilder,
+    resourceEditionComponentBuilder,
+    resourceDeletionComponentBuilder,
+  }: {
+    title: string;
+    path: string;
+    resourceName: string;
+    queryKey: (page: number) => any[];
+    queryFn: (page: number) => () => Promise<any>;
+    columnBuilders: [string, (resource: any) => React.ReactNode][];
+    resourceCreationComponentBuilder: (
+      opened: boolean
+    ) => React.ReactNode;
+    resourceEditionComponentBuilder: (
+      resourceId: string | null
+    ) => React.ReactNode;
+    resourceDeletionComponentBuilder: (
+      resource: any, opened: boolean, onClose: () => void
+    ) => React.ReactNode;
+  }
+) {
+  const [params] = useSearchParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const newResource = params.has('new');
+  const resourceId = params.get(resourceName);
+  const page = params.has('page') ? parseInt(params.get('page') || "") : 1;
+  const { isPending, data, error } = useQuery({
+    queryKey: queryKey(page),
+    queryFn: queryFn(page)
+  });
+
+  if (isPending || !data) {
+    return <div>Loading...</div>;
+  }
+
+  const [resources, pagination] = data;
+
+  const rows = resources.map((resource: any) => (
+    <ResourceTableRow
+      key={resource.id}
+      resourceName={resourceName}
+      resource={resource}
+      rowValueBuilders={columnBuilders.map(([_, valueBuilder]) => valueBuilder)}
+      resourceDeletionComponentBuilder={resourceDeletionComponentBuilder} />
+  ));
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>ID</Table.Th>
+            {
+              columnBuilders.map(([name, _], i) => (
+                <Table.Th key={i}>{name}</Table.Th>
+              ))
+            }
+            <Table.Th>Actions</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {rows}
+        </Table.Tbody>
+      </Table>
+      <Pagination
+        total={pagination.total_pages} value={pagination.page}
+        onChange={(value) => {
+          navigate(addQueryParamToLocation(location, 'page', value.toString()));
+        } } />
+      <Link to={path + '?new'}><Button>Create</Button></Link>
+      {
+        resourceCreationComponentBuilder(newResource)
+      }
+      {
+        resourceEditionComponentBuilder(resourceId)
+      }
+    </div>
+  );
+}
--- frontend/src/features/repos/components/ReposManagement.tsx
+++ frontend/src/features/repos/components/ReposManagement.tsx
@@ -1,95 +1,33 @@
-import { Anchor, Button, Table, Divider, Pagination } from '@mantine/core';
 import { RepoCreation, RepoEdition } from './RepoEditor';
 import { RepoDeletion } from './RepoDeletion';
 import { getRepos } from '../api';
-import { Link, useSearchParams, useLocation, useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { useDisclosure } from '@mantine/hooks';
-import { addQueryParamToLocation } from '@/utils/router';
-
-function RepoTableRow({repo}: {repo: Repo}) {
-  const location = useLocation();
-  const repoLink = addQueryParamToLocation(location, "repo", repo.id);
-  const [deletionModalOpened, {open: openDeletionModal, close: closeDeletionModal}] = useDisclosure();
-
-  return (
-    <Table.Tr>
-      <Table.Td>
-        <Anchor component={Link} to={repoLink}>
-          {repo.id}
-        </Anchor>
-      </Table.Td>
-      <Table.Td>{repo.name}</Table.Td>
-      <Table.Td>{repo.created_at}</Table.Td>
-      <Table.Td>{repo.stats!.first_log_date || "n/a"}</Table.Td>
-      <Table.Td>{repo.stats!.last_log_date || "n/a"}</Table.Td>
-      <Table.Td>{repo.stats!.log_count}</Table.Td>
-      <Table.Td>{repo.stats!.storage_size}</Table.Td>
-      <Table.Td>
-        <Anchor component={Link} to={repoLink}>
-          Edit
-        </Anchor>
-        <Divider size="sm" orientation="vertical" />
-        <Anchor onClick={() => openDeletionModal()}>
-          Delete
-        </Anchor>
-        <RepoDeletion repo={repo} opened={deletionModalOpened} onClose={closeDeletionModal}/>
-      </Table.Td>
-    </Table.Tr>
-  );
-}
+import { ResourceManagement } from '@/components/ResourceManagement';
 
 export function ReposManagement() {
-  const [params] = useSearchParams();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const newRepo = params.has('new');
-  const repoId = params.get('repo');
-  const page = params.has('page') ? parseInt(params.get('page') || "") : 1;
-  const { isPending, data, error } = useQuery({
-    queryKey: ['repos', 'page', page],
-    queryFn: () => getRepos(page, {includeStats: true})
-  });
-
-  if (isPending || !data) {
-    return <div>Loading...</div>;
-  }
-
-  const [repos, pagination] = data;
-
-  const rows = repos.map((repo) => <RepoTableRow key={repo.id} repo={repo}/>);
-
   return (
-    <div>
-      <h1>Repos Management</h1>
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Creation date</Table.Th>
-            <Table.Th>First log date</Table.Th>
-            <Table.Th>Last log date</Table.Th>
-            <Table.Th>Log count</Table.Th>
-            <Table.Th>Storage size</Table.Th>
-            <Table.Th>Actions</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {rows}
-        </Table.Tbody>
-      </Table>
-      <Pagination
-        total={pagination.total_pages} value={pagination.page}
-        onChange={
-          (value) => {
-            navigate(addQueryParamToLocation(location, 'page', value.toString()));
-          }
-        }
-      />
-      <Link to='/repos?new'><Button>Create</Button></Link>
-      <RepoCreation opened={newRepo}/>
-      <RepoEdition repoId={repoId}/>
-    </div>
+    <ResourceManagement
+      title="Repos Management"
+      path="/repos"
+      resourceName="repo"
+      queryKey={(page) => ['repos', 'page', page, {includeStats: true}]}
+      queryFn={(page) => () => getRepos(page, {includeStats: true})}
+      columnBuilders={[
+        ["Name", (repo: Repo) => repo.name],
+        ["Creation date", (repo: Repo) => repo.created_at],
+        ["First log date", (repo: Repo) => repo.stats!.first_log_date || "n/a"],
+        ["Last log date", (repo: Repo) => repo.stats!.last_log_date || "n/a"],
+        ["Log count", (repo: Repo) => repo.stats!.log_count],
+        ["Storage size", (repo: Repo) => repo.stats!.storage_size],
+      ]}
+      resourceCreationComponentBuilder={(opened) => (
+        <RepoCreation opened={opened} />
+      )}
+      resourceEditionComponentBuilder={(resourceId) => (
+        <RepoEdition repoId={resourceId} />
+      )}
+      resourceDeletionComponentBuilder={(resource, opened, onClose) => (
+        <RepoDeletion repo={resource} opened={opened} onClose={onClose} />
+      )}
+    />
   );
 }
