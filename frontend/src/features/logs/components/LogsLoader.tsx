d0041642c22575a7bdb8301dbc27d132ec609813
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-19 18:24:06 +0200

    front: fix log list loading multi-pages coming from previous requests

--- frontend/src/features/logs/components/LogsLoader.tsx
+++ frontend/src/features/logs/components/LogsLoader.tsx
@@ -1,18 +1,38 @@
 import { Button, Center } from '@mantine/core';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import { getLogs, LogsFilterParams } from '../api';
 import { LogsTable } from './LogsTable';
+import { useEffect, useRef } from 'react';
+import deepEqual from 'deep-equal'
 
 export function LogsLoader(
   { filter = {}, onTableFilterChange } :
   { filter: LogsFilterParams; onTableFilterChange: (name: string, value: string) => void; }) {
   const { isPending, error, data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
-    queryKey: ['logs', filter],
+    queryKey: ['logs', 'list', filter],
     queryFn: async ({ pageParam }: { pageParam: string | null; }) => await getLogs(pageParam, filter),
     enabled: !!filter.repoId,
     initialPageParam: null,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
+  const currentFilterRef = useRef<LogsFilterParams | null>(null);
+  const queryClient = useQueryClient();
+
+  // If the user apply filter A, then filter B, then filter A again, he will see the logs of filter A
+  // with result pages already loaded, which can be confusing.
+  // To avoid this, we remove the query for the previous filter when the user applies a different filter.
+  useEffect(() => {
+    if (! deepEqual(filter, currentFilterRef.current)) {
+      queryClient.removeQueries({queryKey: ['logs', 'list', currentFilterRef.current]});
+      currentFilterRef.current = filter;
+    }
+  });
+
+  // See comment above: we need to remove the result for the current filter when the component is unmounted,
+  // because the useEffect above won't have a currentFilterRef if the component is unmounted and remounted.
+  useEffect(() => {
+    return () => queryClient.removeQueries({queryKey: ['logs', 'list', currentFilterRef.current]});
+  }, []);
 
   if (isPending)
     return <div>Loading...</div>;
