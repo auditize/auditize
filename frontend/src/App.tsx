d8b29cb236239784fb109157d4378eadbdd25ff7
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-28 14:33:30 +0200

    front: improve auth handling
    - on the log-in form, redirect toward /logs on log-in success
    - display navigation links depending on the logged-in state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,31 +15,44 @@ import {
   createBrowserRouter,
   RouterProvider,
   Link,
-  Outlet
+  Outlet,
 } from "react-router-dom";
 import { LogInForm } from '@/features/log-in';
+import { useState } from 'react';
 
-function Main() {
+function IfLoggedIn({isLoggedIn, children} : {isLoggedIn: boolean, children: React.ReactNode}) {
+  return isLoggedIn ? <>{children}</> : null;
+}
+
+function IfNotLoggedIn({isLoggedIn, children} : {isLoggedIn: boolean, children: React.ReactNode}) {
+  return !isLoggedIn ? <>{children}</> : null;
+}
+
+function Main({isLoggedIn} : {isLoggedIn: boolean}) {
   return (
     <AppShell header={{height: 60}}>
       <AppShell.Header>
         <Group h="100%" px="md" justify="right">
           <Group ml="xl">
-          <UnstyledButton>
-              <Link to="/log-in">Log-in</Link>
-            </UnstyledButton>
-            <UnstyledButton>
-              <Link to="/logs">Logs</Link>
-            </UnstyledButton>
-            <UnstyledButton>
-              <Link to="/repos">Repository Management</Link>
-            </UnstyledButton>
-            <UnstyledButton>
-              <Link to="/users">User Management</Link>
-            </UnstyledButton>
-            <UnstyledButton>
-              <Link to="/integrations">Integration Management</Link>
-            </UnstyledButton>
+            <IfNotLoggedIn isLoggedIn={isLoggedIn}>
+              <UnstyledButton>
+                <Link to="/log-in">Log-in</Link>
+              </UnstyledButton>
+            </IfNotLoggedIn>
+            <IfLoggedIn isLoggedIn={isLoggedIn}>
+              <UnstyledButton>
+                <Link to="/logs">Logs</Link>
+              </UnstyledButton>
+              <UnstyledButton>
+                <Link to="/repos">Repository Management</Link>
+              </UnstyledButton>
+              <UnstyledButton>
+                <Link to="/users">User Management</Link>
+              </UnstyledButton>
+              <UnstyledButton>
+                <Link to="/integrations">Integration Management</Link>
+              </UnstyledButton>
+            </IfLoggedIn>
           </Group>
         </Group>
       </AppShell.Header>
@@ -59,37 +72,42 @@ export default function App() {
       }
     }
   });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children: [
-        {
-          path: "logs",
-          element: <Logs/>
-        },
-        {
-          path: 'repos',
-          element: <ReposManagement/>
-        },
-        {
-          path: 'users',
-          element: <UsersManagement/>
-        },
-        {
-          path: 'integrations',
-          element: <IntegrationsManagement/>
-        }
-      ],
-    },
+    isLoggedIn ?
+      {
+        path: "/",
+        element: <Main isLoggedIn={isLoggedIn}/>,
+        children: [
+          {
+            path: "logs",
+            element: <Logs/>,
+          },
+          {
+            path: 'repos',
+            element: <ReposManagement/>
+          },
+          {
+            path: 'users',
+            element: <UsersManagement/>
+          },
+          {
+            path: 'integrations',
+            element: <IntegrationsManagement/>
+          }
+        ],
+      } :
+      {},
     {
       path: "/signup/:token",
       element: <Signup/>
     },
     {
       path: "/log-in",
-      element: <LogInForm/>
+      element: <LogInForm onLogged={() => {
+        setIsLoggedIn(true);
+      }}/>
     }
   ]);
 
