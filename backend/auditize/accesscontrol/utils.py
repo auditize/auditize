5d43e12e1b7983f0429f87b42f5c07df5a0feeb6
Nicolas Delon <nicolas.delon@gmail.com>
2024-04-29 12:04:47 +0200

    back: refactor and fix normalize_access_rights function

--- backend/auditize/accesscontrol/utils.py
+++ backend/auditize/accesscontrol/utils.py
@@ -1,4 +1,27 @@
-from auditize.accesscontrol.models import AccessRights, LogPermissions
+from auditize.accesscontrol.models import AccessRights, ReadWritePermissions
+
+
+def _normalize_repo_permissions(
+    repo_perms: dict[str, ReadWritePermissions],
+    global_read: bool = False,
+    global_write: bool = False,
+) -> dict[str, ReadWritePermissions]:
+    if global_read and global_write:  # shortcut / optimization
+        return {}
+
+    normalized: dict[str, ReadWritePermissions] = {}
+
+    for repo_id, repo_perms in repo_perms.items():
+        normalized_repo_perms = repo_perms.model_copy(
+            update={
+                "read": False if global_read else repo_perms.read,
+                "write": False if global_write else repo_perms.write,
+            }
+        )
+        if normalized_repo_perms.read or normalized_repo_perms.write:
+            normalized[repo_id] = normalized_repo_perms
+
+    return normalized
 
 
 def normalize_access_rights(rights: AccessRights) -> AccessRights:
@@ -6,31 +29,11 @@ def normalize_access_rights(rights: AccessRights) -> AccessRights:
         # cleanup all specific permissions
         return AccessRights(is_superadmin=True)
 
-    if rights.logs.read and rights.logs.write:
-        # cleanup permissions on specific repositories
-        return rights.model_copy(
-            update={"logs": LogPermissions(read=True, write=True)}
-        )
-
-    if rights.logs.read:
-        # cleanup read permissions on specific repositories
+    if rights.logs.read or rights.logs.write:
+        # cleanup specific permissions on specific repositories
         return rights.model_copy(
             update={"logs": rights.logs.model_copy(
-                update={"repos": {
-                    repo_id: repo_perms.model_copy(update={"read": False})
-                    for repo_id, repo_perms in rights.logs.repos.items()
-                }}
-            )}
-        )
-
-    if rights.logs.write:
-        # cleanup write permissions on specific repositories
-        return rights.model_copy(
-            update={"logs": rights.logs.model_copy(
-                update={"repos": {
-                    repo_id: repo_perms.model_copy(update={"write": False})
-                    for repo_id, repo_perms in rights.logs.repos.items()
-                }}
+                update={"repos": _normalize_repo_permissions(rights.logs.repos, rights.logs.read, rights.logs.write)}
             )}
         )
 
