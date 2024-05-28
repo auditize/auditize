f670aaaecd199c1d18bf5aa9a2367aac74260735
Nicolas Delon <nicolas.delon@gmail.com>
2024-05-28 18:32:25 +0200

    back: remove dead code from inject-logs2.py

--- backend/scripts/inject-logs2.py
+++ backend/scripts/inject-logs2.py
@@ -5,7 +5,6 @@ import random
 import sys
 import uuid
 from collections import defaultdict
-from itertools import product
 
 import requests
 
@@ -444,87 +443,6 @@ class LogProvider:
         return self._user_creation_log()
 
 
-def format_number(number: int, total: int) -> str:
-    return str(number % total).zfill(len(str(total)))
-
-
-def create_value(prefix: str, counter: int, total: int) -> str:
-    return "%s_%s" % (prefix, format_number(counter, total))
-
-
-def create_log(counter: int):
-    log = {
-        "action": {
-            "category": create_value("action_category", counter, 10),
-            "type": create_value("action_type", counter, 100),
-        },
-        "actor": {
-            "ref": create_value("user", counter, 1000),
-            "type": "user",
-            "name": create_value("user", counter, 1000),
-            "extra": [
-                {
-                    "name": create_value("actor_extra_name", counter, 3),
-                    "value": create_value("actor_extra_value", counter, 10),
-                }
-            ],
-        },
-        "source": [
-            {
-                "name": create_value("source_name", counter, 5),
-                "value": create_value("source_value", counter, 50),
-            }
-        ],
-        "resource": {
-            "ref": create_value("resource", counter, 1000),
-            "type": create_value("resource", counter, 50),
-            "name": create_value("resource", counter, 1000),
-            "extra": [
-                {
-                    "name": create_value("resource_extra_name", counter, 3),
-                    "value": create_value("resource_extra_value", counter, 10),
-                }
-            ],
-        },
-        "details": [
-            {
-                "name": create_value("detail_name", counter, 10),
-                "value": create_value("detail_value", counter, 100),
-            }
-        ],
-        "tags": [
-            {
-                "type": create_value("simple_tag_type", counter, 3),
-            },
-            {
-                "ref": create_value("rich_tag", counter, 1000),
-                "type": create_value("rich_tag_type", counter, 3),
-                "name": create_value("rich_tag_name", counter, 1000),
-            },
-        ],
-        "node_path": [
-            {
-                "ref": create_value("customer", counter, 10),
-                "name": create_value("Customer", counter, 10),
-            },
-            {
-                "ref": create_value(
-                    "customer_%s_entity" % format_number(counter, 10), counter, 100
-                ),
-                "name": create_value("Entity", counter, 100),
-            },
-        ],
-    }
-    attachments = [
-        {
-            "name": "file.txt",
-            "data": b"Hello, World!",
-            "type": "text",
-        }
-    ]
-    return log, attachments
-
-
 def jsonify(data):
     return json.dumps(data, indent=4, ensure_ascii=False)
 
@@ -572,11 +490,6 @@ def main(argv):
         log, attachments = provider.create_log()
         inject_log(base_url, repo_id, api_key, log, attachments)
     print()
-
-
-# def main(argv):
-#     actors = list(generate_node_paths(10))
-#     print(jsonify(actors))
 
 
 if __name__ == "__main__":
