a0c2ee35cecae811ef264cdcef49dcfe04d9816f
Nicolas Delon <nicolas.delon@gmail.com>
2024-06-04 22:25:06 +0200

    front: improve error handling on resource deletion

--- frontend/src/components/ResourceManagement/ResourceDeletion.tsx
+++ frontend/src/components/ResourceManagement/ResourceDeletion.tsx
@@ -2,6 +2,8 @@ import { Box, Button, Group, Modal, Text } from "@mantine/core";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 
+import { InlineErrorMessage } from "../InlineErrorMessage";
+
 interface ResourceDeletionProps {
   message: React.ReactNode;
   opened: boolean;
@@ -41,10 +43,18 @@ export function ResourceDeletion({
             <Button onClick={onClose} size="xs" variant="outline">
               Cancel
             </Button>
-            <Button onClick={() => mutation.mutate()} size="xs" color="red">
+            <Button
+              onClick={() => mutation.mutate()}
+              disabled={mutation.isPending}
+              size="xs"
+              color="red"
+            >
               Delete
             </Button>
           </Group>
+          <InlineErrorMessage>
+            {mutation.error && "Error: " + mutation.error.message}
+          </InlineErrorMessage>
         </Box>
       </div>
     </Modal>
