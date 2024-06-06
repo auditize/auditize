ab851a15b84a577c4b53997c740e34991658e72c
Nicolas Delon <nicolas.delon@gmail.com>
2024-06-06 18:19:32 +0200

    front: add source field columns in logs

--- frontend/src/features/logs/components/LogTable.tsx
+++ frontend/src/features/logs/components/LogTable.tsx
@@ -37,8 +37,44 @@ function InlineFilterLink({
   );
 }
 
+function getCustomFieldValue(
+  fields: CustomField[],
+  fieldName: string,
+): string | null {
+  const field = fields.find((f) => f.name === fieldName);
+  return field ? field.value : null;
+}
+
 function DateField({ log }: { log: Log }) {
   return <Text>{humanizeDate(log.savedAt)}</Text>;
+}
+
+function SourceField({
+  log,
+  fieldName,
+  onTableFilterChange,
+}: {
+  log: Log;
+  fieldName: string;
+  onTableFilterChange: TableFilterChangeHandler;
+}) {
+  if (!log.source) {
+    return null;
+  }
+  const fieldValue = getCustomFieldValue(log.source, fieldName);
+  if (!fieldValue) {
+    return null;
+  }
+
+  return (
+    <InlineFilterLink
+      onClick={() =>
+        onTableFilterChange("source", new Map([[fieldName, fieldValue]]))
+      }
+    >
+      {fieldValue}
+    </InlineFilterLink>
+  );
 }
 
 function ActorField({
@@ -111,14 +147,6 @@ function ActorRefField({
       </InlineFilterLink>
     )
   );
-}
-
-function getCustomFieldValue(
-  fields: CustomField[],
-  fieldName: string,
-): string | null {
-  const field = fields.find((f) => f.name === fieldName);
-  return field ? field.value : null;
 }
 
 function ActorCustomField({
@@ -259,16 +287,17 @@ function ColumnSelector({
 function sortFields(a: string, b: string) {
   const order: { [key: string]: number } = {
     date: 0,
-    actor: 1,
-    actorType: 2,
-    actorName: 3,
-    actorRef: 4,
-    "actor.": 5,
-    actionType: 6,
-    actionCategory: 7,
-    resource: 8,
-    resourceType: 9,
-    node: 10,
+    "source.": 1,
+    actor: 2,
+    actorType: 3,
+    actorName: 4,
+    actorRef: 5,
+    "actor.": 6,
+    actionType: 7,
+    actionCategory: 8,
+    resource: 9,
+    resourceType: 10,
+    node: 11,
   };
   const splitFieldName = (name: string) => {
     const parts = name.split(".");
@@ -295,6 +324,21 @@ function fieldToColumn(
       title: "Date",
       render: (log: Log) => <DateField log={log} />,
     };
+
+  if (field.startsWith("source.")) {
+    const fieldName = field.split(".")[1];
+    return {
+      accessor: `source.${fieldName}`,
+      title: "Source: " + labelize(fieldName),
+      render: (log: Log) => (
+        <SourceField
+          log={log}
+          fieldName={fieldName}
+          onTableFilterChange={onTableFilterChange}
+        />
+      ),
+    };
+  }
 
   if (field === "actor")
     return {
