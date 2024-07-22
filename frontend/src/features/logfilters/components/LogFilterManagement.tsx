dde81427c8775c16ad41d64ff5ef966478d71dda
Nicolas Delon <nicolas.delon@gmail.com>
2024-07-22 17:24:27 +0200

    front: add log filter apply link in log filter management

--- frontend/src/features/logfilters/components/LogFilterManagement.tsx
+++ frontend/src/features/logfilters/components/LogFilterManagement.tsx
@@ -1,6 +1,8 @@
+import { Anchor } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
 import { IconArchive } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 
 import { ResourceManagement } from "@/components/ResourceManagement";
 
@@ -26,7 +28,14 @@ export function LogFilterManagement() {
       queryKey={(search, page) => ["logFilters", search, page]}
       queryFn={(search, page) => () => getLogFilters(search, page)}
       columnBuilders={[
-        [t("log.filter.list.column.name"), (filter: LogFilter) => filter.name],
+        [
+          t("log.filter.list.column.name"),
+          (filter: LogFilter) => (
+            <Anchor component={NavLink} to={`/logs?filterId=${filter.id}`}>
+              {filter.name}
+            </Anchor>
+          ),
+        ],
       ]}
       resourceEditionComponentBuilder={(resourceId, onClose) => (
         <LogFilterEdition filterId={resourceId} onClose={onClose} />
