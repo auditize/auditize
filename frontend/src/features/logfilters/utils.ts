54f403dac04275de6f9d91759e9c1a1e3fe8beb8
Nicolas Delon <nicolas.delon@gmail.com>
2024-07-23 19:53:02 +0200

    front: fix bad serialization of filter columns settings coming from the API

--- frontend/src/features/logfilters/utils.ts
+++ frontend/src/features/logfilters/utils.ts
@@ -2,19 +2,19 @@ import * as changeCase from "change-case";
 
 import { sortFields } from "../logs/components/LogTable";
 
+// Make a special case for custom fields (that contains a ".") because
+// changeCase treats "." as "_" (and we don't want that)
+// it assumes that the custom field group is all lowercase (which is currently true:
+// actor, source, resource, details)
+
 export function normalizeFilterColumnsForApi(columns: string[]): string[] {
-  return columns.toSorted(sortFields).map((column) => {
-    if (column.includes(".")) {
-      // make a special case for custom fields (that contains ".") because
-      // changeCase.snakeCase transforms "." to "_"
-      // it assumes that the custom field group is all lowercase (which is currently true:
-      // actor, source, resource, details)
-      return column;
-    }
-    return changeCase.snakeCase(column);
-  });
+  return columns
+    .toSorted(sortFields)
+    .map((col) => (col.includes(".") ? col : changeCase.snakeCase(col)));
 }
 
 export function unnormalizeFilterColumnsFromApi(columns: string[]): string[] {
-  return columns.map((col) => changeCase.camelCase(col));
+  return columns.map((col) =>
+    col.includes(".") ? col : changeCase.camelCase(col),
+  );
 }
