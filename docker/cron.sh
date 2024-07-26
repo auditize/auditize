128320ef9aad651026162f8dc21f1ebd5d31b3f7
Nicolas Delon <nicolas.delon@gmail.com>
2024-07-26 14:17:48 +0200

    docker: fix command in crontab + add logging to cron container

--- docker/cron.sh
+++ docker/cron.sh
@@ -5,7 +5,8 @@ set -x
 
 declare | grep ^AUDITIZE_ >  /etc/cron.d/auditize
 cat <<EOF >> /etc/cron.d/auditize
-* * * * * root /usr/local/bin/python -m auditize purge_expired_logs >> /etc/cron.d/auditize
+* * * * * root /usr/local/bin/python -m auditize purge_expired_logs >> /var/log/auditize.log 2>&1
 EOF
 
-exec cron -f
+: > /var/log/auditize.log
+exec cron -f & tail -f /var/log/auditize.log
