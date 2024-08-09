6d2fb73bb280b40b51865022efa3d81fffc20370
Nicolas Delon <nicolas.delon@gmail.com>
2024-08-09 16:08:49 +0200

    back: turn scripts/bootstrap-superadmin.py into 'python -m auditize bootstrap-superadmin'

--- backend/src/auditize/__main__.py
+++ backend/src/auditize/__main__.py
@@ -1,12 +1,56 @@
 import argparse
 import asyncio
+import getpass
 import sys
 
 from auditize.database import get_dbm
+from auditize.exceptions import UnknownModelException
 from auditize.log.service import apply_log_retention_period
 from auditize.permissions.models import Permissions
 from auditize.user.models import User
-from auditize.user.service import get_users, hash_user_password, save_user
+from auditize.user.service import (
+    get_user_by_email,
+    get_users,
+    hash_user_password,
+    save_user,
+)
+
+
+def _get_password() -> str:
+    password = getpass.getpass("Password: ")
+    confirm = getpass.getpass("Confirm password: ")
+
+    if password != confirm:
+        print("Passwords do not match, please try again.", file=sys.stderr)
+        print("", file=sys.stderr)
+        return _get_password()
+
+    return password
+
+
+async def bootstrap_superadmin(email: str, first_name: str, last_name: str):
+    dbm = get_dbm()
+
+    password = _get_password()
+
+    try:
+        await get_user_by_email(dbm, email)
+    except UnknownModelException:
+        pass
+    else:
+        sys.exit(f"Error: user with email {email} already exists")
+
+    await save_user(
+        dbm,
+        User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password_hash=hash_user_password(password),
+            permissions=Permissions(is_superadmin=True),
+        ),
+    )
+    print(f"User with email {email} has been successfully created")
 
 
 async def bootstrap_default_superadmin():
@@ -32,16 +76,33 @@ async def purge_expired_logs():
 
 async def main(args):
     parser = argparse.ArgumentParser()
-    sub_parsers = parser.add_subparsers()
+    sub_parsers = parser.add_subparsers(required=True)
+
+    # CMD bootstrap-default-superadmin
     bootstrap_default_superadmin_parser = sub_parsers.add_parser(
         "bootstrap-default-superadmin"
     )
-    bootstrap_default_superadmin_parser.set_defaults(func=bootstrap_default_superadmin)
-    purge_expired_logs_parser = sub_parsers.add_parser("purge-expired-logs")
-    purge_expired_logs_parser.set_defaults(func=purge_expired_logs)
+    bootstrap_default_superadmin_parser.set_defaults(
+        func=lambda _: bootstrap_default_superadmin()
+    )
 
-    args = parser.parse_args(args)
-    await args.func()
+    # CMD bootstrap-superadmin
+    bootstrap_superadmin_parser = sub_parsers.add_parser("bootstrap-superadmin")
+    bootstrap_superadmin_parser.add_argument("email")
+    bootstrap_superadmin_parser.add_argument("first_name")
+    bootstrap_superadmin_parser.add_argument("last_name")
+    bootstrap_superadmin_parser.set_defaults(
+        func=lambda cmd_args: bootstrap_superadmin(
+            cmd_args.email, cmd_args.first_name, cmd_args.last_name
+        )
+    )
+
+    # CMD purge-expired
+    purge_expired_logs_parser = sub_parsers.add_parser("purge-expired-logs")
+    purge_expired_logs_parser.set_defaults(func=lambda _: purge_expired_logs())
+
+    parsed_args = parser.parse_args(args)
+    await parsed_args.func(parsed_args)
 
     return 0
 
