d0f26037f354f93d1ff1bf9ce2c289f5ea1d8487
Nicolas Delon <nicolas.delon@gmail.com>
2024-08-09 10:55:59 +0200

    tests: improve mock patch in test_cli.py

--- backend/tests/test_cli.py
+++ backend/tests/test_cli.py
@@ -4,15 +4,22 @@ import pytest
 
 from auditize.__main__ import main
 from auditize.database import DatabaseManager
+from conftest import dbm as dbm_fixt
 from helpers.http import HttpTestHelper
 from helpers.user import PreparedUser
 
 pytestmark = pytest.mark.anyio
 
 
+@pytest.fixture()
+def dbm(dbm_fixt):
+    # Override dbm fixture from conftest to also patch auditize.__main__.get_dbm
+    with patch("auditize.__main__.get_dbm", lambda: dbm_fixt):
+        yield dbm_fixt
+
+
 async def test_empty_db(dbm: DatabaseManager):
-    with patch("auditize.__main__.get_dbm", lambda: dbm):
-        await main(["bootstrap_superadmin"])
+    await main(["bootstrap_superadmin"])
     client = HttpTestHelper.spawn()
     resp = await client.assert_post_ok(
         "/auth/user/login",
@@ -23,6 +30,5 @@ async def test_empty_db(dbm: DatabaseManager):
 
 async def test_not_empty_db(dbm: DatabaseManager, user: PreparedUser):
     assert await dbm.core_db.users.count_documents({}) == 1
-    with patch("auditize.__main__.get_dbm", lambda: dbm):
-        await main(["bootstrap_superadmin"])
+    await main(["bootstrap_superadmin"])
     assert await dbm.core_db.users.count_documents({}) == 1
