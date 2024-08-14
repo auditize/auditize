0e25962860ccbb85af713f39774f7af1cb02968d
Nicolas Delon <nicolas.delon@gmail.com>
2024-08-14 18:28:05 +0200

    front: add 8 min characters constraint on password change form

--- frontend/src/components/PasswordForm.tsx
+++ frontend/src/components/PasswordForm.tsx
@@ -1,11 +1,30 @@
-import { isNotEmpty, matchesField } from "@mantine/form";
+import { hasLength, isNotEmpty, matchesField } from "@mantine/form";
 import { useTranslation } from "react-i18next";
+
+function combineValidators(...validators: ((value: string) => any)[]) {
+  return (value: string) => {
+    for (const validator of validators) {
+      const error = validator(value);
+      if (error) {
+        return error;
+      }
+    }
+
+    return null;
+  };
+}
 
 export function usePasswordValidation() {
   const { t } = useTranslation();
 
   return {
-    password: isNotEmpty(t("common.passwordForm.password.required")),
+    password: combineValidators(
+      isNotEmpty(t("common.passwordForm.password.required")),
+      hasLength(
+        { min: 8 },
+        t("common.passwordForm.password.tooShort", { min: 8 }),
+      ),
+    ),
     passwordConfirmation: matchesField(
       "password",
       t("common.passwordForm.passwordConfirmation.doesNotMatch"),
