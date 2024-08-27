054703a4b03c0db3e5b5b806073b00a14e58a1de
Nicolas Delon <nicolas.delon@gmail.com>
2024-08-27 17:58:08 +0200

    back: move log consolidation related functions to auditize.log.service.consolidation

--- backend/src/auditize/log/service/__init__.py
+++ backend/src/auditize/log/service/__init__.py
@@ -1,16 +1,11 @@
-from .csv import (
-    CSV_BUILTIN_COLUMNS,
-    get_logs_as_csv,
-    validate_csv_columns,
-)
-from .main import (
-    apply_log_retention_period,
-    get_log,
+from .consolidation import (
+    check_log,
+    consolidate_log,
+    consolidate_log_attachment,
     get_log_action_categories,
     get_log_action_types,
     get_log_actor_extra_fields,
     get_log_actor_types,
-    get_log_attachment,
     get_log_attachment_mime_types,
     get_log_attachment_types,
     get_log_detail_fields,
@@ -20,6 +15,16 @@ from .main import (
     get_log_resource_types,
     get_log_source_fields,
     get_log_tag_types,
+)
+from .csv import (
+    CSV_BUILTIN_COLUMNS,
+    get_logs_as_csv,
+    validate_csv_columns,
+)
+from .main import (
+    apply_log_retention_period,
+    get_log,
+    get_log_attachment,
     get_logs,
     save_log,
     save_log_attachment,
