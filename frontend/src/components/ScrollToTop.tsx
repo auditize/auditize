0aede819a87a2e525e4da2b268a183ab90f4d7c5
Nicolas Delon <nicolas.delon@gmail.com>
2024-09-26 22:07:19 +0200

    front: fix 'Scroll to top' button displayed at the same level as log details modal

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -11,8 +11,10 @@ export function ScrollToTop() {
   const [scroll, scrollTo] = useWindowScroll();
   const { t } = useTranslation();
 
+  // NB: set zIndex to 100 instead of the default 200 to avoid displaying the button
+  // at the exact same level as modals (the log detail modal for example)
   return (
-    <Affix position={{ bottom: 15, right: 20 }}>
+    <Affix position={{ bottom: 15, right: 20 }} zIndex={100}>
       <Transition transition="slide-up" mounted={scroll.y > viewportHeight}>
         {(transitionStyles) => (
           <Button
