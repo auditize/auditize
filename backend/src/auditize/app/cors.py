390609d69ad02632658da1ea6f0493c8889d55eb
Nicolas Delon <nicolas.delon@gmail.com>
2024-10-03 11:04:52 +0200

    [back] Remove no-longer relevant comment: app instance is now loaded through a function and CORS can be put under tests (even if it's not currently performed)

--- backend/src/auditize/app/cors.py
+++ backend/src/auditize/app/cors.py
@@ -9,9 +9,6 @@ def setup_cors(app: FastAPI):
     if not config.cors_allow_origins:
         return
 
-    # FIXME: CORS is currently untested as the app is loaded once the module is imported
-    # putting this under tests would require to to build the app through a function.
-    # Please note that is directly supported by uvicorn through the --factory option.
     app.add_middleware(
         CORSMiddleware,
         allow_origins=config.cors_allow_origins,
