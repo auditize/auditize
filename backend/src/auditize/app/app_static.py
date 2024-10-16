df5a61e8a0fee20841d0847a23ed9135d0a6ab64
Nicolas Delon <nicolas.delon@gmail.com>
2024-10-16 21:13:04 +0200

    [back] Fix tests when running on a clean checkout
    In this case, the auditize.data.html directory does not exist.

--- backend/src/auditize/app/app_static.py
+++ backend/src/auditize/app/app_static.py
@@ -19,7 +19,7 @@ async def _index_html_redirection(request, call_next):
 
 def build_app(*, cors_allow_origins: list[str]):
     app = FastAPI(openapi_url=None)
-    app.mount("", StaticFiles(directory=_STATIC_DIR))
+    app.mount("", StaticFiles(directory=_STATIC_DIR, check_dir=False))
     app.add_middleware(BaseHTTPMiddleware, dispatch=_index_html_redirection)
     setup_cors(app, cors_allow_origins=cors_allow_origins)
     return app
