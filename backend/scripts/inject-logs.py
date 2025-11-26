5bd2ac7a6fbe08626b8d43a033fe5a1880d4b89d
Nicolas Delon <nicolas.delon@gmail.com>
2025-11-26 09:55:00 +0100

    [scripts] Make inject-logs.py roadworthy

--- backend/scripts/inject-logs.py
+++ backend/scripts/inject-logs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import json
+import os
 import sys
 
-import requests
+import httpx
 
 
 def format_number(number: int, total: int) -> str:
@@ -11,14 +12,14 @@ def format_number(number: int, total: int) -> str:
 
 
 def create_value(prefix: str, counter: int, total: int) -> str:
-    return "%s_%s" % (prefix, format_number(counter, total))
+    return "%s-%s" % (prefix, format_number(counter, total))
 
 
 def create_log(counter: int):
     log = {
         "action": {
-            "category": create_value("action_category", counter, 10),
-            "type": create_value("action_type", counter, 100),
+            "category": create_value("action-category", counter, 10),
+            "type": create_value("action-type", counter, 100),
         },
         "actor": {
             "ref": create_value("user", counter, 1000),
@@ -26,15 +27,15 @@ def create_log(counter: int):
             "name": create_value("user", counter, 1000),
             "extra": [
                 {
-                    "name": create_value("actor_extra_name", counter, 3),
-                    "value": create_value("actor_extra_value", counter, 10),
+                    "name": create_value("actor-extra-name", counter, 3),
+                    "value": create_value("actor-extra-value", counter, 10),
                 }
             ],
         },
         "source": [
             {
-                "name": create_value("source_name", counter, 5),
-                "value": create_value("source_value", counter, 50),
+                "name": create_value("source-name", counter, 5),
+                "value": create_value("source-value", counter, 50),
             }
         ],
         "resource": {
@@ -43,25 +44,25 @@ def create_log(counter: int):
             "name": create_value("resource", counter, 1000),
             "extra": [
                 {
-                    "name": create_value("resource_extra_name", counter, 3),
-                    "value": create_value("resource_extra_value", counter, 10),
+                    "name": create_value("resource-extra-name", counter, 3),
+                    "value": create_value("resource-extra-value", counter, 10),
                 }
             ],
         },
         "details": [
             {
-                "name": create_value("detail_name", counter, 10),
-                "value": create_value("detail_value", counter, 100),
+                "name": create_value("detail-name", counter, 10),
+                "value": create_value("detail-value", counter, 100),
             }
         ],
         "tags": [
             {
-                "type": create_value("simple_tag_type", counter, 3),
+                "type": create_value("simple-tag-type", counter, 3),
             },
             {
-                "ref": create_value("rich_tag", counter, 1000),
-                "type": create_value("rich_tag_type", counter, 3),
-                "name": create_value("rich_tag_name", counter, 1000),
+                "ref": create_value("rich-tag", counter, 1000),
+                "type": create_value("rich-tag-type", counter, 3),
+                "name": create_value("rich-tag-name", counter, 1000),
             },
         ],
         "entity_path": [
@@ -92,26 +93,26 @@ def jsonify(data):
 
 
 def inject_log(base_url, repo_id, api_key, log, attachments):
-    resp = requests.post(
-        f"{base_url}/repos/{repo_id}/logs",
+    resp = httpx.post(
+        f"{base_url}/api/repos/{repo_id}/logs",
         headers={"Authorization": f"Bearer " + api_key},
         json=log,
     )
-    if not resp.ok:
+    if not resp.is_success:
         sys.exit(
             "Error %s while pushing log:\n%s" % (resp.status_code, jsonify(resp.json()))
         )
     log_id = resp.json()["id"]
     for attachment in attachments:
-        resp = requests.post(
-            f"{base_url}/repos/{repo_id}/logs/{log_id}/attachments",
+        resp = httpx.post(
+            f"{base_url}/api/repos/{repo_id}/logs/{log_id}/attachments",
             headers={"Authorization": f"Bearer " + api_key},
             files={"file": (attachment["name"], attachment["data"])},
             data={
                 "type": attachment["type"],
             },
         )
-        if not resp.ok:
+        if not resp.is_success:
             sys.exit(
                 "Error %s while pushing attachment: %s"
                 % (resp.status_code, jsonify(resp.json()))
@@ -119,22 +120,24 @@ def inject_log(base_url, repo_id, api_key, log, attachments):
 
 
 def main(argv):
-    if len(argv) != 4:
-        sys.exit("Usage: %s COUNT REPO_ID API_KEY" % argv[0])
+    if len(argv) != 2:
+        sys.exit("Usage: %s COUNT" % argv[0])
 
-    base_url = "http://localhost:8000"
+    try:
+        base_url = os.environ["AUDITIZE_URL"]
+        repo_id = os.environ["AUDITIZE_REPO"]
+        api_key = os.environ["AUDITIZE_APIKEY"]
+    except KeyError as e:
+        sys.exit("Missing environment variable: %s" % e)
+
     count = int(argv[1])
-    repo_id = argv[2]
-    api_key = argv[3]
 
     for i in range(count):
         print("Inject log %d of %d" % (i + 1, count), end="\r")
-        log, attachments = create_log(i)
+        log, attachments = create_log(i + 1)
         inject_log(base_url, repo_id, api_key, log, attachments)
     print()
 
 
 if __name__ == "__main__":
     main(sys.argv)
-    if len(sys.argv) != 4:
-        sys.exit("Usage: %s COUNT REPO_ID API_KEY" % sys.argv[0])
