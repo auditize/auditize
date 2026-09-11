auditize/auditize

commit 78bb00303e3af4cd4e9236c3c1ae6e8c78fc3f96

[front] Improve display on pages displaying an empty list

frontend/src/features/log/components/Logs.tsx
@@ -10,11 +10,12 @@ import {
 import { useDocumentTitle } from "@mantine/hooks";
 import { IconEdit, IconLogs, IconRestore, IconX } from "@tabler/icons-react";
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { Trans, useTranslation } from "react-i18next";
+import { Link, useNavigate } from "react-router-dom";
 
 import Message from "@/components/Message";
 import { ScrollToTop } from "@/components/ScrollToTop";
+import { useAuthenticatedUser } from "@/features/auth";
 import { LogFilterEdition, LogFilterFavoriteIcon } from "@/features/log-filter";
 import { LogFilter, useLogFilterMutation } from "@/features/log-filter/api";
 import { useLogRepoListQuery } from "@/features/repo";
@@ -42,14 +43,24 @@ export function BaseLogs({
     setSelectedColumns,
     logComponentRef,
   } = useLogNavigationState();
+  const { currentUser } = useAuthenticatedUser();
 
   if (repoListQuery.data && repoListQuery.data.length === 0) {
     return (
-      <Flex justify="center">
-        <Message.Warning textProps={{ size: "md" }}>
-          {t("log.list.noRepos")}
-        </Message.Warning>
-      </Flex>
+      <Message.Warning alertProps={{ style: { maxWidth: "fit-content" } }}>
+        {t("log.list.noRepos")}
+
+        {currentUser.permissions.management.repos.read &&
+          currentUser.permissions.management.repos.write && (
+            <>
+              <br />
+              <Trans i18nKey="log.list.createRepoShortcut">
+                You can create a repository by clicking
+                <Link to="/repos">here</Link>.
+              </Trans>
+            </>
+          )}
+      </Message.Warning>
     );
   } else {
     return (

frontend/src/components/Message.tsx
@@ -1,4 +1,4 @@
-import { Alert, Text, TextProps } from "@mantine/core";
+import { Alert, AlertProps, Text, TextProps } from "@mantine/core";
 import {
   IconAlertTriangle,
   IconCheckbox,
@@ -8,6 +8,7 @@ import React from "react";
 
 interface MessageProps {
   title?: string;
+  alertProps?: AlertProps;
   textProps?: TextProps;
   children: React.ReactNode;
 }
@@ -16,17 +17,25 @@ export function Message({
   color,
   icon,
   title,
+  alertProps,
   textProps,
   children,
 }: {
   color?: string;
   icon: React.ReactNode;
   title?: string;
+  alertProps?: AlertProps;
   textProps?: TextProps;
   children: React.ReactNode;
 }) {
   return (
-    <Alert title={title} variant="light" color={color} icon={icon}>
+    <Alert
+      title={title}
+      variant="light"
+      color={color}
+      icon={icon}
+      {...alertProps}
+    >
       <Text size="sm" {...textProps}>
         {children}
       </Text>
@@ -36,12 +45,18 @@ export function Message({
 
 export default Message;
 
-function MessageSuccess({ title, textProps, children }: MessageProps) {
+function MessageSuccess({
+  title,
+  alertProps,
+  textProps,
+  children,
+}: MessageProps) {
   return (
     <Message
       color="green"
       icon={<IconCheckbox />}
       title={title}
+      alertProps={alertProps}
       textProps={textProps}
       children={children}
     />
@@ -50,12 +65,13 @@ function MessageSuccess({ title, textProps, children }: MessageProps) {
 
 Message.Success = MessageSuccess;
 
-function MessageInfo({ title, textProps, children }: MessageProps) {
+function MessageInfo({ title, alertProps, textProps, children }: MessageProps) {
   return (
     <Message
       // Primary color
       icon={<IconInfoSquare />}
       title={title}
+      alertProps={alertProps}
       textProps={textProps}
       children={children}
     />
@@ -64,12 +80,18 @@ function MessageInfo({ title, textProps, children }: MessageProps) {
 
 Message.Info = MessageInfo;
 
-function MessageWarning({ title, textProps, children }: MessageProps) {
+function MessageWarning({
+  title,
+  alertProps,
+  textProps,
+  children,
+}: MessageProps) {
   return (
     <Message
       color="orange"
       icon={<IconAlertTriangle />}
       title={title}
+      alertProps={alertProps}
       textProps={textProps}
       children={children}
     />

frontend/src/components/ResourceManagement/ResourceManagement.tsx
@@ -16,6 +16,7 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { ApiErrorMessage } from "../ErrorMessage";
+import Message from "../Message";
 import { SearchInput } from "../SearchInput";
 import cssClasses from "./ResourceManagement.module.css";
 import { useResourceManagementState } from "./ResourceManagementState";
@@ -140,6 +141,7 @@ type ColumnDefinition =
 export function ResourceManagement({
   title,
   name,
+  emptyStateMessage,
   stateMode = "url",
   queryKey,
   queryFn,
@@ -150,6 +152,7 @@ export function ResourceManagement({
 }: {
   title: React.ReactNode;
   name: string;
+  emptyStateMessage?: string;
   stateMode?: "url" | "useState";
   queryKey: (search: string | null, page: number) => any[];
   queryFn: (search: string | null, page: number) => () => Promise<any>;
@@ -179,6 +182,7 @@ export function ResourceManagement({
 
   let rows;
   let pagination;
+  let hasResources: boolean | undefined;
   if (resourcesQuery.isPending) {
     rows = Array.from({ length: 10 }).map((_, rowIndex) => (
       <Table.Tr key={rowIndex} style={{ height: "2rem" }}>
@@ -201,6 +205,9 @@ export function ResourceManagement({
         resourceDeletionComponentBuilder={resourceDeletionComponentBuilder}
       />
     ));
+    // If there is a search, we consider there are existing resources even
+    // if we don't actually know.
+    hasResources = search ? true : resources.length > 0;
   }
 
   return (
@@ -208,8 +215,20 @@ export function ResourceManagement({
       <Title order={1} pb="xl" fw={550} size="26">
         {title}
       </Title>
+      {hasResources === false && emptyStateMessage && (
+        <Message.Info
+          alertProps={{
+            style: { maxWidth: "fit-content", whiteSpace: "pre-line" },
+            mb: "md",
+          }}
+        >
+          {emptyStateMessage}
+        </Message.Info>
+      )}
       <Group justify="space-between" pb="md">
-        <Search value={search} onChange={setSearch} />
+        {hasResources !== false && (
+          <Search value={search} onChange={setSearch} />
+        )}
         {resourceCreationComponentBuilder && (
           <Button
             onClick={() => setIsNew(true)}
@@ -219,28 +238,30 @@ export function ResourceManagement({
           </Button>
         )}
       </Group>
-      <Stack align="center">
-        <Table highlightOnHover withTableBorder verticalSpacing="sm">
-          <Table.Thead
-            style={{ backgroundColor: "var(--auditize-header-color)" }}
-          >
-            <Table.Tr>
-              {columnDefinitions.map(([name, _, style], i) => (
-                <Table.Th style={style} key={i}>
-                  {name}
-                </Table.Th>
-              ))}
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>{rows}</Table.Tbody>
-        </Table>
-        <Pagination
-          total={pagination?.total_pages}
-          value={pagination?.page}
-          onChange={setPage}
-          py="md"
-        />
-      </Stack>
+      {hasResources !== false && (
+        <Stack align="center">
+          <Table highlightOnHover withTableBorder verticalSpacing="sm">
+            <Table.Thead
+              style={{ backgroundColor: "var(--auditize-header-color)" }}
+            >
+              <Table.Tr>
+                {columnDefinitions.map(([name, _, style], i) => (
+                  <Table.Th style={style} key={i}>
+                    {name}
+                  </Table.Th>
+                ))}
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+          <Pagination
+            total={pagination?.total_pages}
+            value={pagination?.page}
+            onChange={setPage}
+            py="md"
+          />
+        </Stack>
+      )}
       {resourceCreationComponentBuilder &&
         resourceCreationComponentBuilder(isNew, () => setIsNew(false))}
       {resourceEditionComponentBuilder(resourceId, () => setResourceId(null))}

frontend/src/features/apikey/components/ApikeyManagement.tsx
@@ -33,6 +33,7 @@ export function ApikeysManagement() {
         </>
       }
       name={t("apikey.apikey")}
+      emptyStateMessage={t("apikey.list.emptyStateMessage")}
       queryKey={(search, page) => ["apikeys", "list", search, page]}
       queryFn={(search, page) => () => getApikeys(search, page)}
       columnDefinitions={[

frontend/src/features/log-i18n-profile/components/LogI18nProfileManagement.tsx
@@ -35,6 +35,7 @@ export function LogI18nProfileManagement() {
         </>
       }
       name={t("logi18nprofile.logi18nprofile")}
+      emptyStateMessage={t("logi18nprofile.list.emptyStateMessage")}
       queryKey={(search, page) => ["logi18nprofiles", "list", search, page]}
       queryFn={(search, page) => () => getLogI18nProfiles(search, page)}
       columnDefinitions={[

frontend/src/features/repo/components/RepoManagement.tsx
@@ -91,6 +91,7 @@ export function RepoManagement() {
         </>
       }
       name={t("repo.repo")}
+      emptyStateMessage={t("repo.list.emptyStateMessage")}
       queryKey={(search, page) => [
         "repos",
         "list",

frontend/src/i18n/en.ts
@@ -62,6 +62,8 @@ export default {
         lastLog: "Last log",
         viewLogs: "View logs",
       },
+      emptyStateMessage:
+        "No repositories have been created yet.\nRepositories are used to store logs, you must create one to start logging into Auditize.",
     },
     delete: {
       confirm:
@@ -97,6 +99,8 @@ export default {
         langs: "Languages",
       },
       noLangs: "none",
+      emptyStateMessage:
+        "No log translation profiles have been created yet.\nThe log translation profiles allow you to translate the log content into different languages.",
     },
     delete: {
       confirm:
@@ -152,6 +156,8 @@ export default {
         name: "Name",
         permissions: "Permissions",
       },
+      emptyStateMessage:
+        "No API keys have been created yet.\nAn API key is required to authenticate requests to the API when sending logs to Auditize for instance.",
     },
     form: {
       name: {
@@ -232,6 +238,8 @@ export default {
       documentTitle: "Logs",
       noResults: "No results",
       noRepos: "No repositories have been created yet.",
+      createRepoShortcut:
+        "You can create a repository by clicking <1>here</1>.",
       loadMore: "Load more",
       untilMustBeGreaterThanSince:
         "The end date must be greater than the start date",

frontend/src/i18n/fr.ts
@@ -63,6 +63,8 @@ export default {
         lastLog: "Dernier journal",
         viewLogs: "Voir les journaux",
       },
+      emptyStateMessage:
+        "Aucun dépôt de journaux n'a encore été créé.\nLes dépôts de journaux sont utilisés pour stocker les journaux, vous devez en créer un pour commencer à envoyer des journaux à Auditize.",
     },
     delete: {
       confirm:
@@ -98,6 +100,8 @@ export default {
         langs: "Langues",
       },
       noLangs: "aucune",
+      emptyStateMessage:
+        "Aucun profil de traduction des journaux n'a encore été créé.\nLes profils de traduction des journaux permettent de traduire le contenu des journaux en différentes langues.",
     },
     delete: {
       confirm:
@@ -154,6 +158,8 @@ export default {
         name: "Nom",
         permissions: "Permissions",
       },
+      emptyStateMessage:
+        "Aucune clé d'API n'a encore été créée.\nLes clés d'API permettent d'authentifier les requêtes à l'API lors de l'envoi de journaux à Auditize par exemple.",
     },
     form: {
       name: {
@@ -235,6 +241,8 @@ export default {
       },
       documentTitle: "Journaux",
       noRepos: "Aucun dépôt de journaux n'a encore été créé",
+      createRepoShortcut:
+        "Vous pouvez créer un dépôt de journaux en cliquant <1>ici</1>.",
       noResults: "Pas de résultats",
       loadMore: "Voir plus",
       untilMustBeGreaterThanSince:

commit 215b37bdd2a75e3fdb7a91a6ae026cf0b235c92d

[front] Fix web-component not displaying (regression introduced in 78bb0030)

frontend/src/features/log/components/Logs.tsx
@@ -1,7 +1,6 @@
 import {
   ActionIcon,
   Center,
-  Flex,
   Group,
   rem,
   Stack,
@@ -35,8 +34,6 @@ export function BaseLogs({
   withLogFilters?: boolean;
   withScrollToTop?: boolean;
 }) {
-  const { t } = useTranslation();
-  const repoListQuery = useLogRepoListQuery({ enabled: withRepoSearchParam });
   const {
     searchParams,
     setSearchParams,
@@ -44,58 +41,36 @@ export function BaseLogs({
     setSelectedColumns,
     logComponentRef,
   } = useLogNavigationState();
-  const { currentUser } = useAuthenticatedUser();
 
-  if (repoListQuery.data && repoListQuery.data.length === 0) {
-    return (
-      <Center pt="xl">
-        <Message.Info alertProps={{ style: { maxWidth: "fit-content" } }}>
-          {t("log.list.noRepos")}
-
-          {currentUser.permissions.management.repos.read &&
-            currentUser.permissions.management.repos.write && (
-              <>
-                <br />
-                <Trans i18nKey="log.list.createRepoShortcut">
-                  You can create a repository by clicking
-                  <Link to="/repos">here</Link>.
-                </Trans>
-              </>
-            )}
-        </Message.Info>
-      </Center>
-    );
-  } else {
-    return (
-      <Stack ref={logComponentRef} gap="md">
-        <LogNavigation
-          params={searchParams}
-          onChange={(newSearchParams) => {
-            setSearchParams(newSearchParams);
-          }}
-          selectedColumns={selectedColumns}
-          withRepoSearchParam={withRepoSearchParam}
-          withLogFilters={withLogFilters}
-        />
-        <LogTable
-          searchParams={searchParams}
-          onTableSearchParamChange={(name, value) => {
-            setSearchParams(
-              LogSearchParams.fromProperties({
-                repoId: searchParams.repoId,
-                since: searchParams.since,
-                until: searchParams.until,
-                [name]: value,
-              }),
-            );
-          }}
-          selectedColumns={selectedColumns}
-          onSelectedColumnsChange={setSelectedColumns}
-        />
-        {withScrollToTop && <ScrollToTop />}
-      </Stack>
-    );
-  }
+  return (
+    <Stack ref={logComponentRef} gap="md">
+      <LogNavigation
+        params={searchParams}
+        onChange={(newSearchParams) => {
+          setSearchParams(newSearchParams);
+        }}
+        selectedColumns={selectedColumns}
+        withRepoSearchParam={withRepoSearchParam}
+        withLogFilters={withLogFilters}
+      />
+      <LogTable
+        searchParams={searchParams}
+        onTableSearchParamChange={(name, value) => {
+          setSearchParams(
+            LogSearchParams.fromProperties({
+              repoId: searchParams.repoId,
+              since: searchParams.since,
+              until: searchParams.until,
+              [name]: value,
+            }),
+          );
+        }}
+        selectedColumns={selectedColumns}
+        onSelectedColumnsChange={setSelectedColumns}
+      />
+      {withScrollToTop && <ScrollToTop />}
+    </Stack>
+  );
 }
 
 function LogFilterFavoriteAction({ filter }: { filter: LogFilter }) {
@@ -244,7 +219,10 @@ function LogTitle() {
 }
 
 export function Logs() {
+  const { currentUser } = useAuthenticatedUser();
+  const repoListQuery = useLogRepoListQuery();
   const { filter, isFilterDirty } = useLogNavigationState();
+  const { t } = useTranslation();
 
   return (
     <div>
@@ -255,7 +233,26 @@ export function Logs() {
           <LogTitle />
         )}
       </Title>
-      <BaseLogs />
+      {repoListQuery.data && repoListQuery.data.length === 0 ? (
+        <Center pt="xl">
+          <Message.Info alertProps={{ style: { maxWidth: "fit-content" } }}>
+            {t("log.list.noRepos")}
+
+            {currentUser.permissions.management.repos.read &&
+              currentUser.permissions.management.repos.write && (
+                <>
+                  <br />
+                  <Trans i18nKey="log.list.createRepoShortcut">
+                    You can create a repository by clicking
+                    <Link to="/repos">here</Link>.
+                  </Trans>
+                </>
+              )}
+          </Message.Info>
+        </Center>
+      ) : (
+        <BaseLogs />
+      )}
     </div>
   );
 }